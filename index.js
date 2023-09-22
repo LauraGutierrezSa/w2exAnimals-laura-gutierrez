b72059fcfa1854910150982c32a51f5b66ab9ca7
Laura Gutierrez <lauragsanahuja@gmail.com>
2023-09-22 12:16:19 +0200

    Add getAnimalSpieces function

---
+++ index.js
@@ -0,0 +1,18 @@
+const firstAnimal = {
+  id: 1,
+  name: "Dodo",
+  age: 20,
+  weight: 15,
+  spieces: "Bird",
+  extintionYear: 1662,
+};
+const secondAnimal = {
+  id: 2,
+  name: "Quokka",
+  age: 5,
+  weight: 6,
+  spieces: "Mammal",
+  extintionYear: null,
+};
+
+const getAnimalSpieces = (animal) => animal.spieces;
