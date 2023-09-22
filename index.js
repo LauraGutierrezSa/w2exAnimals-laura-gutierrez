f9ef4a21dd97bfdfb555749ec4c81f3d63fffa81
Laura Gutierrez <lauragsanahuja@gmail.com>
2023-09-22 12:41:27 +0200

    refactor isAnimalExtinct function.

--- index.js
+++ index.js
@@ -12,16 +12,15 @@ const secondAnimal = {
   age: 5,
   weight: 6,
   spieces: "Mammal",
-  extintionYear: null,
 };
 
 const getAnimalSpieces = (animal) => animal.spieces;
 
 const isAnimalExtint = (animal) => {
-  if (animal.extintionYear === null) {
-    return "This animal is not extint.";
+  if (animal.extintionYear) {
+    return true;
   } else {
-    return "This animal is extint.";
+    return false;
   }
 };
 
@@ -29,4 +28,4 @@ const getAnimalDescription = (animal) => {
   return `The animal ${animal.name} is a ${animal.spieces} o ${animal.age} years old and weights ${animal.weight} kg.`;
 };
 
-console.log(getAnimalDescription(secondAnimal));
+console.log(isAnimalExtint(firstAnimal));
